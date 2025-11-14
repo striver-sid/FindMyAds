94a772fce833455c0bdb7bcbcc8fb8e40d94fa30
striver-sid <sidduprofessional012@gmail.com>
2025-11-14 22:34:10 +0530

    created the websocket

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from typing import Union
-from fastapi import FastAPI
+from fastapi import FastAPI,WebSocket
 app=FastAPI()
 
 @app.get("/")
 def printsomething():
     print("Server is on")
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept() # Accept client connection
+    while True:
+        data = await websocket.receive_text() # Receive message
+        print("Client says:", data)
+        await websocket.send_text(f"You wrote: {data}") # Send response
